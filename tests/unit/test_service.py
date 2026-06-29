364c7e5957989e397c2b83e9e66286b39b8fe948
georgiiviktorov <nowayisleft@gmail.com>
2026-06-29 13:57:38 +0700

    feat: use case test

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -1,13 +1,66 @@
 import pytest
 
-from flights.domain.errors import (FlightDeparted, FlightNotFound,
-                                   NotReservationOwner)
-from flights.domain.model import FlightStatus
+from flights.domain.commands import CreateFlight, ReserveSeat
+from flights.domain.errors import FlightAlreadyExists
 from flights.infrastructure.uow.memory.memory_uow import InMemoryUnitOfWork
+from flights.infrastructure.repo.memory.memory_repository import InMemoryRepository
 from flights.service_layer.service import FlightService
 
 
-def create_flight_service():
-    uow = InMemoryUnitOfWork()
-    flight_service = FlightService(uow)
-    return flight_service
+def create_unit_of_work():
+    uow = InMemoryUnitOfWork(InMemoryRepository())
+    return uow
+
+
+def create_command_create_flight():
+    cmd = CreateFlight(
+        flight_id="SU-901",
+        seat_ids=["A1", "A2", "A3"],
+    )
+    return cmd
+
+
+def create_command_reserve_seat():
+    cmd = ReserveSeat(
+        flight_id="SU-901",
+        passenger_id="p01",
+        seat_id="A1"
+    )
+    return cmd
+
+
+def test_create_flight_success():
+    uow = create_unit_of_work()
+    service = FlightService(uow)
+    cmd = create_command_create_flight()
+    service.create_flight(cmd)
+
+    assert uow.flights.get(flight_id="SU-901").flight_id == cmd.flight_id
+
+
+def test_create_flight_already_exists_failure():
+    uow = create_unit_of_work()
+    service = FlightService(uow)
+    cmd = create_command_create_flight()
+    service.create_flight(cmd)
+
+    with pytest.raises(FlightAlreadyExists):
+        service.create_flight(cmd)
+
+
+def test_reserve_seat_success():
+    uow = create_unit_of_work()
+    service = FlightService(uow)
+
+    cmd = create_command_create_flight()
+    service.create_flight(cmd)
+
+    cmd = create_command_reserve_seat()
+
+    service.reserve_seat(cmd)
+
+    flight = uow.flights.get(flight_id="SU-901")
+    seat = next(s for s in flight.seats if s.seat_id == cmd.seat_id)
+
+    assert seat.passenger_id == cmd.passenger_id
+    assert seat.is_reserved
